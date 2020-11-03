9a8392f6d93174f05a5f7076469c8eb027e177e3
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-11-03 13:11:42 +0530

    add iot

---
+++ krdsresourcedemopy.py
@@ -0,0 +1,20 @@
+import boto3
+iotanalytics = boto3.client("iotanalytics")
+
+def handler(event, context):
+    try:
+        data = iotanalytics.batch_put_message(
+            channelName="kiot",
+            messages=[
+                {
+                    'messageId': "1",
+                    'payload': "Test"
+                }
+            ]
+        )
+        print(data)
+    except BaseException as e:
+        print(e)
+        raise(e)
+    
+    return {"message": "Successfully executed"}
